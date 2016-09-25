072b62331b080120cfce640da7dff8397f57673c
Maurício Pimenta <mjbpimenta@gmail.com>
2016-09-25 08:25:58 -0300

    add dialog to get phone number.

--- app/src/main/java/com/github/fidelity/lio/merchant/ui/DetailActivity.java
+++ app/src/main/java/com/github/fidelity/lio/merchant/ui/DetailActivity.java
@@ -1,8 +1,12 @@
 package com.github.fidelity.lio.merchant.ui;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.digits.sdk.android.AuthCallback;
 import com.digits.sdk.android.AuthConfig;
@@ -21,28 +25,40 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
         ButterKnife.bind(this);
-
-        AuthConfig.Builder authConfigBuilder = new AuthConfig.Builder()
-                .withAuthCallBack(new AuthCallback() {
-                    @Override
-                    public void success(DigitsSession session, String phoneNumber) {
-
-                    }
-
-                    @Override
-                    public void failure(DigitsException error) {
-
-                    }
-                })
-                .withPhoneNumber("+5521967319761");
-
-        Digits.authenticate(authConfigBuilder.build());
-
     }
 
     @OnClick(R.id.finishButton)
     public void finishButtonOnClick(View view) {
+        startDialog();
+    }
 
+    private void startDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.phone_number_dialog, null);
+        dialogBuilder.setView(dialogView);
+
+        Button smsReceiver = (Button) dialogView.findViewById(R.id.smsReceiver);
+        smsReceiver.setOnClickListener(view -> {
+            AuthConfig.Builder authConfigBuilder = new AuthConfig.Builder()
+                    .withAuthCallBack(new AuthCallback() {
+                        @Override
+                        public void success(DigitsSession session, String phoneNumber) {
+
+                        }
+
+                        @Override
+                        public void failure(DigitsException error) {
+
+                        }
+                    })
+                    .withPhoneNumber("+5521967319761");
+
+            Digits.authenticate(authConfigBuilder.build());
+        });
+
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
     }
 
 }
